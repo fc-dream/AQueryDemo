d98f230c962017d66c5d0ccdaa162f4bdd312537
Christos Peppas <christos.peppas@lbi.com>
2012-07-18 01:47:52 +0100

    revert to normal image loading on article view

--- src/com/lbi/aquerydemo/activities/FeedArticleActivity.java
+++ src/com/lbi/aquerydemo/activities/FeedArticleActivity.java
@@ -1,12 +1,15 @@
 package com.lbi.aquerydemo.activities;
 
 import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxStatus;
+import com.androidquery.callback.BitmapAjaxCallback;
 import com.lbi.aquerydemo.R;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class FeedArticleActivity extends Activity {
     private AQuery $;
@@ -25,8 +28,7 @@ public class FeedArticleActivity extends Activity {
             $.id(R.id.feed_main_title).text(extras.getString("feedTitle"));
             $.id(R.id.feed_description).text(extras.getString("feedDescription"));
             $.id(R.id.feed_pubdate).text(extras.getString("feedPubDate"));
-            Bitmap preset = $.getCachedImage(extras.getString("feedMainImageUrl"));            
-            $.id(R.id.feed_main_image).image(extras.getString("feedMainImageUrl"), true, true, 0, R.drawable.ic_launcher, preset, AQuery.FADE_IN, AQuery.RATIO_PRESERVE);
+            $.id(R.id.feed_main_image).image(extras.getString("feedMainImageUrl"), true, true, 0, R.drawable.ic_launcher);            
         }
 
     }
